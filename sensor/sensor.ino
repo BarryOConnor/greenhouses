95afd254b6fef2dfc1fee9763b14308a76a9e419
BarryOConnor <me@barryoconnor.co.uk>
2023-02-21 09:48:37 +0000

    fixed bug where ESP01s would wait every hour

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -8,23 +8,28 @@
 #define ONEMINUTE 6e7   // 1 minute (in microseconds)
 #define ONEHOUR 60      // 60 minutes in an hour
 #define TIMEOUT 20000   // wait 20 seconds for timeout to website
-#define DEBUG true     // display debug messages
-#define MAXRETRIES 10
+#define DEBUG true      // display debug messages
+#define MAXRETRIES 30
 #define CMDWAIT 100
 #define CMDSEND 101
 #define MIDNIGHT 24
-#define RTCMEMORYSTART 66
+#define RTCMEMORYSTART 64
+#define HOURS 0
+#define COMMAND 1
 
-extern "C" {
-#include "user_interface.h" // this is for the RTC memory read/write functions
-}
+
+// data[0] will be the hours, data[1] will be the command
+typedef struct {
+  uint32_t crc32;
+  byte data[2];
+} RTCStore;
 
 typedef struct {
-  int command = 000;
   int hour = -1;
-} rtcStore;
+  int minutes = -1;
+} TimeValue;
 
-rtcStore rtcMem;
+RTCStore rtcData;
 
 const char* ssid = ***REMOVED***;
 const char* password = ***REMOVED***;
@@ -35,8 +40,23 @@ char humidity[7];
 const char authorization_id[9] = "***REMOVED***";
 const int greenhouse_id = 1;
 double delayMinutes = 0;
+bool dataValid = false;
 
 DHT dht(DHTPIN, DHTTYPE);
+
+uint32_t calculateCRC32(const uint8_t *data, size_t length) {
+  uint32_t crc = 0xffffffff;
+  while (length--) {
+    uint8_t c = *data++;
+    for (uint32_t i = 0x80; i > 0; i >>= 1) {
+      bool bit = crc & 0x80000000;
+      if (c & i) { bit = !bit; }
+      crc <<= 1;
+      if (bit) { crc ^= 0x04c11db7; }
+    }
+  }
+  return crc;
+}
 
 void getSensorData(char *temp, char *humidity){
   // collect data
@@ -47,31 +67,59 @@ void getSensorData(char *temp, char *humidity){
 }
 
 void readFromRTCMemory() {
-  system_rtc_mem_read(RTCMEMORYSTART, &rtcMem, sizeof(rtcMem));
   #if DEBUG
-    Serial.println("Reading from memory...");
-    Serial.print("hour = ");
-    Serial.println(rtcMem.hour);
-    Serial.print("cmd = ");
-    Serial.println(rtcMem.command);
+    Serial.println("-----readFromRTCMemory-----");
   #endif
-  yield();
+
+  if (ESP.rtcUserMemoryRead(0, (uint32_t *)&rtcData, sizeof(rtcData))) {
+    #if DEBUG
+      Serial.println("Reading memory : ");
+    #endif
+
+    uint32_t crcOfData = calculateCRC32((uint8_t *)&rtcData.data[0], sizeof(rtcData.data));
+
+    if (crcOfData != rtcData.crc32) {
+      #if DEBUG
+        Serial.println("CRC32 memory does not match CRC32 of data. Invalid!");
+      #endif
+      dataValid = false;
+    } else {
+      #if DEBUG
+        Serial.println("CRC32 check ok, data is valid.");
+        Serial.print("hour: ");
+        Serial.println(rtcData.data[HOURS]);
+        Serial.print("cmd: ");
+        Serial.println(rtcData.data[COMMAND]);
+      #endif
+      dataValid = false;
+    }
+  }
 }
 
 void writeToRTCMemory() {
-  system_rtc_mem_write(RTCMEMORYSTART, &rtcMem, 8);
-
   #if DEBUG
-    Serial.println("Writing to memory...");
-    Serial.print("hour = ");
-    Serial.println(rtcMem.hour);
-    Serial.print("cmd = ");
-    Serial.println(rtcMem.command);
+  Serial.println("-----writeToRTCMemory-----");
   #endif
-  yield();
+  
+  // Update CRC32 of data
+  rtcData.crc32 = calculateCRC32((uint8_t *)&rtcData.data[0], sizeof(rtcData.data));
+  // Write struct to RTC memory
+  if (ESP.rtcUserMemoryWrite(0, (uint32_t *)&rtcData, sizeof(rtcData))) {
+    #if DEBUG
+      Serial.println("Write Data: ");
+      Serial.print("hour: ");
+      Serial.println(rtcData.data[HOURS]);
+      Serial.print("cmd: ");
+      Serial.println(rtcData.data[COMMAND]);
+    #endif
+  }
 }
 
 void startWiFi(){
+  #if DEBUG
+    Serial.println("-----startWiFi-----");
+  #endif
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -85,7 +133,7 @@ void startWiFi(){
   }
 
   // put the ESP to sleep after too many unsuccessful retries to conserve battery
-  if (count == MAXRETRIES) ESP.deepSleepInstant(ONEHOUR * ONEMINUTE);
+  if (count == MAXRETRIES || WiFi.localIP().toString()=="0.0.0.0") ESP.deepSleepInstant(ONEHOUR * ONEMINUTE);
 
   #if DEBUG
     Serial.println("");
@@ -95,45 +143,70 @@ void startWiFi(){
 }
 
 
-void getNTPTime(){
- 
+TimeValue getNTPTime(){
+  #if DEBUG
+    Serial.println("-----getNTPTime-----");
+  #endif
+
   WiFiUDP ntpUDP;
   NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 0);
   timeClient.begin();
-  timeClient.update();
+  while (!timeClient.isTimeSet()){
+    timeClient.update();
+    delay(1000);
+  }
 
   #if DEBUG
     Serial.print("Current time is: ");
-    Serial.print(timeClient.getHours());
-    Serial.print(":");
-    Serial.println(timeClient.getMinutes());
+    Serial.println(timeClient.getFormattedTime());
+  #endif
+  return {timeClient.getHours(), timeClient.getMinutes()};
+}
+
+void setNextWakeTime(){
+  #if DEBUG
+  Serial.println("-----setNextWakeTime-----");
   #endif
 
+  TimeValue time = getNTPTime();
 
+  readFromRTCMemory(); 
 
-
-  if ((rtcMem.command != CMDWAIT && rtcMem.command != CMDSEND) || rtcMem.hour == timeClient.getHours()) {
-    // past the hour so we can send 
-    rtcMem.command = CMDSEND;
+  if (!dataValid) {
+    // data stored isnt valid so must be a power on rather than deep sleep
+    rtcData.data[COMMAND] = CMDSEND;
   } else {
-    rtcMem.command = CMDWAIT;
+    if(rtcData.data[HOURS] == time.hour) { 
+      rtcData.data[COMMAND] = CMDWAIT;
+    } else {
+      rtcData.data[COMMAND] = CMDSEND;
+    }
   }
 
-  rtcMem.hour = timeClient.getHours() + 1;
-  if (rtcMem.hour > 23) rtcMem.hour = 0;
+  if(rtcData.data[COMMAND] == CMDSEND){
+    rtcData.data[HOURS] = time.hour + 1;
+    if (rtcData.data[HOURS] > 23) rtcData.data[HOURS] = 0;
+  }
 
-  delayMinutes = ONEHOUR - timeClient.getMinutes();
+  delayMinutes = ONEHOUR - time.minutes;
+
+  #if DEBUG
+    Serial.print("hour: ");
+    Serial.println(rtcData.data[HOURS]);
+    Serial.print("command: ");
+    Serial.println(rtcData.data[COMMAND]);
+  #endif  
+
+  
 }
 
 void setup() {
-  delay(5000);  
-  readFromRTCMemory(); 
-
-  startWiFi();
-  getNTPTime();
-
-  dht.begin();
   Serial.begin(115200);
+  
+  startWiFi();
+  setNextWakeTime();
+  
+  dht.begin();  
 }
 
 void loop() {
@@ -141,8 +214,8 @@ void loop() {
   however, it is useful to use the loop to keep allow for retry attempts on sending data before sleeping
   as the interval between sleep/wake cycles may be high, we need this to make sure we send information before sleeping.
   */
-  if(rtcMem.command == CMDSEND) {
-    rtcMem.command = CMDWAIT;
+  if(rtcData.data[COMMAND] == CMDSEND) {
+    rtcData.data[COMMAND] = CMDWAIT;
 
     #if DEBUG
       Serial.println();
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php 
+date_default_timezone_set('Europe/London');
+$rightnow = date("Y-m-d H:i:s"); 
+echo $rightnow?>
